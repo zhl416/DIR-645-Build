055cbba119a98d0ec32c41073113d5450795bbaa
zhl416 <zhl_416@126.com>
2025-01-12 01:43:08 +0800

    Update board.h

--- configs/DIR-645/board.h
+++ configs/DIR-645/board.h
@@ -1,1 +1,21 @@
-
+#define BOARD_PID "DIR-645"
+#define BOARD_NAME "DIR-645"
+#define BOARD_DESC "D-Link DIR-645 Wireless Router"
+#define BOARD_VENDOR_NAME "D-Link Corporation"
+#define BOARD_VENDOR_URL "http://www.dlink.com/"
+#define BOARD_MODEL_URL "http://www.dlink.com/products/?pid=DIR-645"
+#define BOARD_BOOT_TIME 30
+#define BOARD_FLASH_TIME 120
+#define BOARD_GPIO_BTN_RESET 13
+#define BOARD_GPIO_BTN_WPS   26
+#define BOARD_GPIO_LED_POWER "gpio0"
+#define BOARD_GPIO_LED_WAN   "gpio19"
+#define BOARD_GPIO_LED_WIFI  "gpio14"
+#define BOARD_HAS_5G_11AC 0
+#define BOARD_NUM_ANT_5G_TX 0
+#define BOARD_NUM_ANT_5G_RX 0
+#define BOARD_NUM_ANT_2G_TX 2
+#define BOARD_NUM_ANT_2G_RX 2
+#define BOARD_NUM_ETH_LEDS 0
+#define BOARD_HAS_EPHY_L1000 1
+#define BOARD_HAS_EPHY_W1000 1
